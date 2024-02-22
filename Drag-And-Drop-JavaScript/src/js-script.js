00e1d60c90dce79b63bc937876ed688c432c827a
Riad Thalji, PMP <59248461+rthalji@users.noreply.github.com>
2024-02-22 17:00:30 +0300

    Add Comments

--- Drag-And-Drop-JavaScript/src/js-script.js
+++ Drag-And-Drop-JavaScript/src/js-script.js
@@ -1,5 +1,12 @@
-	const numIcons = 19;
-        const ProcNames = {
+	// Drag and Drop Game in Pure JavaScript.
+	// Developed by Trainer Riad Thalji for educational game development.
+	// However it can be used for any Drag and Drop Games.
+
+	// Define number of icons to be draged.
+	const numIcons = 19;   
+        
+	// ProcNames or you may call it Icon Text List will contain the text that will appear on each icon.
+	const ProcNames = {
             p1: "Develop Project Charter",
             p2: "Develop Project Management Plan",
             p3: "Direct and Manage Project Work",
@@ -20,8 +27,10 @@
             p18: "Develop Schedule",
             p19: "Control Schedule"
         };
-
+ 
+	// This function will dipict the table of icons and targets once the game is started or re-started
         function depictTable() {
+	    // Depect the top row of the table:
             document.write("<div class='row'>" +
                 "<div class='th1 col-2'>&nbsp;</div>" +
                 "<div class='th1 col-2'>Initiation</div>" +
@@ -31,6 +40,9 @@
                 "<div class='th1 col-2'>Closing</div>" +
                 "</div>");
 
+	    // This is a high mechanisim where you can make some of the target cells blank and define others as targets.
+	    // Now depect each row of the target cells.  A cell with "0" in it will not be a target.
+	    // From this example in row 3 the second cell will be blank while the third will contain "p8" which is the  name of the correct expected icon to be dragged into it.
             var strRows = depictRows("Integration", "p1", "p2", "p3", "p5", "p7") +
                 	  depictRows("0", "0", "0", "p4", "p6", "0") +
                           depictRows("Scope", "0", "p8", "0", "p12", "0") +
@@ -45,6 +57,7 @@
             document.write(strRows);
         }
 
+ 	// Once called this function will actually depict the Rows:
         function depictRows(leftTitle, v1, v2, v3, v4, v5) {
             const emptyLcDat = "<div class='emptyLc col-2'>&nbsp;</div>";
             const argAry = ["0", v1, v2, v3, v4, v5];
@@ -70,6 +83,7 @@
             return str1;
         }
 
+	// This function will depict all the icons to be dragged and sort them randomly:
         function depictIcons() {
             var strBtn = "";
             var btnAry = [];
@@ -86,10 +100,13 @@
             document.write(strBtn);
         }
 
+	// This function will trigger when the button labled "Start Over" button is clicked
         function restartAll() {
             location.reload();
         }
 
+	// This function will show the correct answer expected by the player so that he can undestand what is expected and compare his answers.
+	// It will fire once the button labled "Peek to Answer" is clicked.
         function peek2Answers() {
             location.reload;
             for (var i = 1; i <= numIcons; i++) {
@@ -100,17 +117,20 @@
             }
         }
 
-        function lookupKey(pProcName) {
+        // This function will get the key name from the icon list (in this example the process names list shown at the beginning of the code):
+	function lookupKey(pProcName) {
             for (var i = 0; i < numIcons; i++) {
                 var theKey = "p" + i;
                 if (ProcNames[theKey] == pProcName) { return theKey; };
             }
         }
 
+	// This function will enable dropping icons into the target cell:
         function allowDrop(ev) {
             ev.preventDefault();
         }
 
+	// This function will process the dragging of an icon:
         function drag(ev) {
             const psrc = document.getElementById(ev.target.id);
             const psrcParent = psrc.parentElement.id;
@@ -119,6 +139,7 @@
             ev.dataTransfer.setData("text", ev.target.id);
         }
 
+	// This function will process the dropping of an icon:
         function drop(ev) {
             ev.preventDefault();
             const data = ev.dataTransfer.getData("text");
@@ -137,6 +158,7 @@
             else { document.getElementById(ptrg.id).style.backgroundColor = "red"; };
         }
 
+	// This function processes the move of an element:
         function moveElement(p1, p2) {
             p1Parent = p1.parentElement;
             p1Parent.removeChild(p1);
